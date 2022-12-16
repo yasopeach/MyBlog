3a3838815fcc0611dfddbaa0319ca2551858f583
canberk <cnbrk@hotmail.com>
2022-12-16 21:24:50 +0300

    Migration işlemi yapıldı, tablolar oluşturuldu.

--- DataAccessLayer/Concrete/Context.cs
+++ DataAccessLayer/Concrete/Context.cs
@@ -15,6 +15,17 @@ namespace DataAccessLayer.Concrete
             optionsBuilder.UseSqlServer("server =DESKTOP-AFUMI0D\\MSSQLCNBRK;database=MyBlogDb; integrated security=true;");
         }
         public DbSet<About> Abouts { get; set; }
+
+        public DbSet<Blog> Blogs { get; set; }
+
+        public DbSet<Category> Categories { get; set; }
+
+        public DbSet<Comment> Comments { get; set; }
+
+        public DbSet<Contact> Contacts { get; set; }
+
+        public DbSet<Writer> Writers { get; set; }
+
     }
 }
 
