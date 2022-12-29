e2229546c15e9e51fda67f294fd75a5dbd2b073c
canberk <cnbrk@hotmail.com>
2022-12-29 22:00:58 +0300

    Devamını oku sayfası partial view yapıldı

---
+++ MyBlog/Controllers/CommentController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyBlog.Controllers
+{
+	public class CommentController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+		
+		public PartialViewResult PartialAddComment()
+		{
+			return PartialView();
+		}
+		public PartialViewResult CommentListByBlog()
+		{
+			return PartialView();
+		}
+	}
+}
